8ceecd935a385fd4ae6db06d3c1a10e347b57168
Shreyans207 <75474973+Shreyans207@users.noreply.github.com>
2021-01-25 20:44:37 +0530

    Update box.js

--- box.js
+++ box.js
@@ -1,26 +1,27 @@
 class Box{
-    constructor(x, y){
-      var options = {
-        'restitution':0.8,
-        'density':1.0,
-        isStatic : false 
+  constructor(x, y, width, height) {
+      var options = {   
+          restitution :0.8,
+          friction : 1,
+          density : 20,
+          isStatic : false,
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
     }
-    this.body = Bodies.rectangle(x, y,30,40, options);
-    this.width = 30;
-    this.height = 40;
-    World.add(world, this.body);
-    }
-
     display(){
-      var pos =this.body.position;
+      var pos= this.body.position;
       var angle = this.body.angle;
       push();
-      translate(this.body.position.x, this.body.position.y);
+      translate(pos.x, pos.y);
       rotate(angle);
       rectMode(CENTER);
-      rect(0,0,30,40);
+      strokeWeight(4);
+      stroke("black");
+      fill("red")
+      rect(0,0,this.width, this.height);
       pop();
     }
-  
-  }
-  
+}
